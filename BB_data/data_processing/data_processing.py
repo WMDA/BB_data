72f584cc9582773d31e722ac811ed7c81eb1e4a5
wmda <p3dh0001@gmail.com>
2022-02-28 16:47:37 +0000

    added data_exploration and data_processing scripts

--- BB_data/data_processing/data_processing.py
+++ BB_data/data_processing/data_processing.py
@@ -1,3 +1,11 @@
 import pandas as pd
-from decouple import config 
-import numpy as np
+from decouple import config
+import re
+import os
+
+print(os.environ)
+
+t2 = config('t2')
+
+df = pd.read_csv(f'{t2}/questionnaire_data.csv')
+df.rename(columns=lambda x: re.sub(r'\D','', x), inplace=True)
